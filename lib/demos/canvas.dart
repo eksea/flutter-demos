6c61747e5b99bb3ff508c9633b3129dc6abc437e
eksea <eksea@outlook.com>
2018-06-27 23:48:22 +0800

    add demo for drawer

---
+++ lib/demos/canvas.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DemosCanvas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Canvas Demo')
+      ),
+      body: Center(child: Text('Canvas Page'),)
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'demos/canvas.dart';
+
+void main() => runApp(Demos());
+
+class Demos extends StatelessWidget {
+  final appTitle = 'Flutter Demos';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      home: DemosHomePage(title: appTitle),
+    );
+  }
+}
+
+class DemosHomePage extends StatelessWidget {
+  final String title;
+
+  DemosHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(child: Text('Home Page!')),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('All Demos'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Canvas'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                  new MaterialPageRoute(
+                    builder: (context) {
+                      return new DemosCanvas();
+                    }
+                  )
+                );
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
